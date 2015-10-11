fcd4f9b636c251be342dbace6e1f3946a95e7ade
Nguyen Hai Chau <nhchau@gmail.com>
2015-10-11 20:29:46 +0700

    Change margins

--- R/myutils.r
+++ R/myutils.r
@@ -10,8 +10,8 @@
 #' naValueGraph(data=df, percent=50)
 
 naValueGraph <- function(data, percent=50, msg="NA values map of") {
-	dy = max(nchar(names(data)))
-	dx = max(nchar(rownames(data)))
+	dy <- max(nchar(names(data)))
+	dx <- max(nchar(rownames(data)))
 	nr <- nrow(data)
 	nc <- ncol(data)
 	tmp <- seq(1, nr, by=100/percent)
@@ -19,11 +19,13 @@ naValueGraph <- function(data, percent=50, msg="NA values map of") {
 		tmp = c(tmp, nr)
 	lbl <- rownames(data)[tmp]
 	nr <- length(lbl)
-	par(mar=c(dx+1, dy/2, 4, 4))
-	image(is.na(data), axes=FALSE, col=c('gray', 'black'),
-		xlab="Observation")
+	dfl <- par("mar")
+	dfl[1] <- dx/2+3
+	dfl[2] <- dy/2
+	par(mar=dfl, cex=0.8)
+	image(is.na(data), axes=FALSE, col=c('gray', 'black'), xlab="Observation")
+	title(paste0(msg, " '", deparse(substitute(data)), "'"))
 	axis(side=1, at=0:(nr-1)/(nr-1), labels=lbl, las=2)
 	axis(side=2, at=0:(nc-1)/(nc-1), labels=colnames(data), las=1)
-	title(paste0(msg, " '", deparse(substitute(data)), "'"))
 }
 
